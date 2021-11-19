300c1736bc2851192709efc02209b71b48c6f101
Zoo Sky <zoosky@gmail.com>
2021-11-19 11:25:57 +0100

    header template

--- src/header/header.js
+++ src/header/header.js
@@ -1,14 +1,12 @@
+import header_template from "./header.html"
 /**
  * header
  */
+
  const header = document.createElement("div");
  header.className = "header";
  
- header.innerHTML = `
-     <img src="./logo.png" class="app-logo" />
-     <h1 class="app-name">Contact App</h1>
-     <img src="./add.png" class="add-icon" />
-   `;
+ header.innerHTML = header_template;
  
  document.body.appendChild(header);
  
